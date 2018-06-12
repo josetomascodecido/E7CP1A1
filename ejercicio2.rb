ca6416421d4622830f476bbc3250c0bff91cc7f9
Jose Tomas <josetomascodecido@gmail.com>
2018-06-12 19:12:32 -0400

    exercise2

--- ejercicio2.rb
+++ ejercicio2.rb
@@ -1,3 +1,6 @@
 productos = {'bebida' => 850, 'chocolate' => 1200, 'galletas' => 900, 'leche' => 750}
 productos['bebida'] = 2000
+productos.delete('galletas')
 productos.each { |producto, valor| puts producto }
+
+nuevalista = productos.to_a
