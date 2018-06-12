1640ca512992016016e15f5639467a452e06b76b
Jose Tomas <josetomascodecido@gmail.com>
2018-06-12 19:35:43 -0400

    exercise4

---
+++ ejercicio3.rb
@@ -0,0 +1,15 @@
+h = {x:1, y:2}
+#Agregar el string z con el valor 3.
+h[:z] = 3
+print h
+#Cambiar el valor de x por 5.
+h[:x] = 5
+print h
+#Eliminar la clave y.
+h.delete(:y)
+print h
+#Si el hash tiene una clave llamada z mostrar en pantalla "yeeah".
+print 'yeaaah' if h.has_key?(:z)
+#Invertir el diccionario de forma que los valores sean las llaves y las llaves los valores
+h = h.invert
+ print h
